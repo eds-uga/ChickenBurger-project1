53f6724bc0693976b28779b4da13075716a7a943
BahaaEddin AlAila <bahaelaila7@gmail.com>
2016-08-20 17:46:15 -0400

    Implementing tag count, but document duplication needs to be done

--- nb.py
+++ nb.py
@@ -1,5 +1,7 @@
 from __future__ import print_function
 from pyspark import SparkContext
+#from nltk import word_tokenize
+
 sc = SparkContext("local[*]","Naive Bayes", pyFiles = ['nb.py'])
 
 X = sc.textFile('data/X_train_vsmall.txt')
@@ -10,6 +12,11 @@ def splitter(x):
 Xsplit = X.flatMap(splitter) #not map!
 Xreduced = Xsplit.reduceByKey(lambda x,y: x+y)
 
-Xreduced.foreach(print)
 VocabularySize = Xreduced.count()
-print(VocabularySize)
+#print(VocabularySize)
+
+def labelSplitter(x):
+    return [(tag,1) for tag in x.upper().split(',') if tag in {'MCAT','CCAT','ECAT','GCAT'}]
+Ysplit = Y.flatMap(labelSplitter)
+Yreduced= Ysplit.reduceByKey(lambda x,y: x+y)
+Yreduced.foreach(print)
