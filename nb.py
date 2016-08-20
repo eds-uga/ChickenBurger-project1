1370188db256cc38ffec4b09959f52eea1762495
BahaaEddin AlAila <bahaelaila7@gmail.com>
2016-08-20 18:07:50 -0400

    Simulating the duplicates

--- nb.py
+++ nb.py
@@ -6,7 +6,20 @@ sc = SparkContext("local[*]","Naive Bayes", pyFiles = ['nb.py'])
 
 X = sc.textFile('data/X_train_vsmall.txt')
 Y = sc.textFile('data/y_train_vsmall.txt')
+D=X.zip(Y)
 
+def documentProcessor(x):
+    (document,labels) = x
+    cleanLabels = [label for label in labels.upper().split(',') if label in {'MCAT','CCAT','ECAT','GCAT'}]
+    cleanWords = [(w,len(cleanLabels)) for w in document.lower().split(' ')]
+    return (cleanWords,cleanLabels)
+
+Dcurated = D.map(documentProcessor)
+Dcurated.foreach(print)
+
+import sys
+
+sys.exit(0)
 def splitter(x):
     return [(w,1) for w in x.lower().split(' ')] #better tokenizer + stopwords removal
 Xsplit = X.flatMap(splitter) #not map!
