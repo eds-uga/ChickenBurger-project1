add5b576aca75bc3f3a032d5ae584dd53bdc925a
BahaaEddin AlAila <bahaelaila7@gmail.com>
2016-08-20 16:48:18 -0400

    Code cleaned and lambda instead of function for reduceByKey

--- nb.py
+++ nb.py
@@ -8,11 +8,7 @@ Y = sc.textFile('data/y_train_vsmall.txt')
 def splitter(x):
     return [(w,1) for w in x.lower().split(' ')] #better tokenizer + stopwords removal
 Xsplit = X.flatMap(splitter) #not map!
-#Xreduced = Xsplit.groupByKey()#.map(lambda x:(x[0],list(x[1]))) 
-#Xreduced.foreach(print)
-def accumulator(x,y):
-    return x+y
-Xreduced = Xsplit.reduceByKey(accumulator)
+Xreduced = Xsplit.reduceByKey(lambda x,y: x+y)
 
 Xreduced.foreach(print)
 VocabularySize = Xreduced.count()
