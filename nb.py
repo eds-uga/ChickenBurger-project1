074bfbdfbc0b75903e846f7770b6c743645fc705
Shawn1776 <zshawn1776@gmail.com>
2016-08-22 17:29:44 -0400

    package training code into a function

--- nb.py
+++ nb.py
@@ -6,33 +6,40 @@ sc = SparkContext("local[*]","Naive Bayes", pyFiles = ['nb.py'])
 
 X = sc.textFile('data/X_train_vsmall.txt')
 Y = sc.textFile('data/y_train_vsmall.txt')
-D=X.zip(Y)
 
-def documentProcessor(x):
-    (document,labels) = x
-    cleanLabels = [label for label in labels.upper().split(',') if label in {'MCAT','CCAT','ECAT','GCAT'}]
-    cleanWords = [(w,{label:1 for label in cleanLabels}) for w in document.lower().split(' ')]
-    return (cleanWords,cleanLabels)
 
-def catAccumulator(x,y):
-    for key in y:
-        if key in x:
-            x[key]+= y[key]
-        else:
-            x[key]=y[key]
-    return x
-Dcurated = D.map(documentProcessor)
-wordCountByCat = Dcurated.flatMap(lambda x: x[0])#.groupByKey().map(lambda x: (x[0],list(x[1])))
-wordCountByCat = wordCountByCat.reduceByKey(catAccumulator)
-#wordCountByCat.foreach(print)
+def naive_bayes_train(xRDD,yRDD): # maybe pass a tokenizer for filtering on line 18
+    dRDD=xRDD.zip(yRDD)	
+#(wordCountByCatRDD, catCount)=naive_bayes_train(trainingDatasetDocsRDD,trainingDatasetLabelsRDD)
+    def documentProcessor(x):
+        (document,labels) = x
+        cleanLabels = [label for label in labels.upper().split(',') if label in {'MCAT','CCAT','ECAT','GCAT'}]
+        cleanWords = [(w,{label:1 for label in cleanLabels}) for w in document.lower().split(' ')]
+        return (cleanWords,cleanLabels)
 
+    def catAccumulator(x,y):
+        for key in y:
+            if key in x:
+                x[key]+= y[key]
+            else:
+                x[key]=y[key]
+        return x
+    DcuratedRDD = dRDD.map(documentProcessor)
+    wordCountByCatRDD = DcuratedRDD.flatMap(lambda x: x[0])#.groupByKey().map(lambda x: (x[0],list(x[1])))
+    wordCountByCatRDD = wordCountByCatRDD.reduceByKey(catAccumulator)
+    #wordCountByCat.foreach(print)    
+    catCountRDD = DcuratedRDD.flatMap(lambda x :[(tag,1) for tag in x[1]]).reduceByKey(lambda x,y:x+y)# -> [("c", c_num) , ("m",m_num), ~~~, ~~~]
+    return wordCountByCatRDD,catCountRDD
+
+
+(wordCountByCatRDD,catCountRDD) = naive_bayes_train(X,Y)
 testDocument = u"Thursday night in the Oval Office with"
 testDocument2 = u"Friday Morning out of the Square Swimming Pool without"
 testRDD = sc.parallelize([testDocument,testDocument2]).zipWithIndex() # (testDocument, 0)
 testRDDSplit=testRDD.flatMap(lambda x: [(w,x[1]) for w in  x[0].lower().split(' ')])
 #testRDDSplit.foreach(print)
 
-jointRDD = testRDDSplit.join(wordCountByCat)     #(uga,0).join((uga,{})) => (uga, (0,{})) 
+jointRDD = testRDDSplit.join(wordCountByCatRDD)  #(uga,0).join((uga,{})) => (uga, (0,{})) 
 #wordCountByCat.join(testRDDSplit)     #(uga,{}).join((uga,0)) => (uga, ({},0))
 
 #jointRDD.foreach(print)
@@ -46,14 +53,8 @@ docIDFirstRDD=jointRDD.map(lambda x: (x[1][0], (x[0],x[1][1])))
 docRDD = docIDFirstRDD.groupByKey().map(lambda x: (x[0],list(x[1])))
 #docRDD.foreach(print)
 
-def labelSplitter(x):
-    return [(tag,1) for tag in x.upper().split(',') if tag in {'MCAT','CCAT','ECAT','GCAT'}]
-Ysplit = Y.flatMap(labelSplitter)
-Yreduced= Ysplit.reduceByKey(lambda x,y: x+y)
 
-#Yreduced.foreach(print)
-
-catCount = dict(Yreduced.collect())
+catCount = dict(catCountRDD.collect())
 
 def sumDictValues(d):
     s = 0
