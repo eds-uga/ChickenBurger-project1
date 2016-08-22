c300771a45532e478362355e1eeac1c6d4fb47ac
Shawn1776 <zshawn1776@gmail.com>
2016-08-22 18:07:38 -0400

    package prediction function code

--- nb.py
+++ nb.py
@@ -32,78 +32,72 @@ def naive_bayes_train(xRDD,yRDD): # maybe pass a tokenizer for filtering on line
     return wordCountByCatRDD,catCountRDD
 
 
+
+
+def naive_bayes_predict (testRDD,wordCountByCatRDD, catCountRDD):
+
+    testRDDSplit=testRDD.flatMap(lambda x: [(w,x[1]) for w in  x[0].lower().split(' ')])
+    #testRDDSplit.foreach(print)
+
+    jointRDD = testRDDSplit.join(wordCountByCatRDD)  #(uga,0).join((uga,{})) => (uga, (0,{})) 
+    #wordCountByCat.join(testRDDSplit)     #(uga,{}).join((uga,0)) => (uga, ({},0))
+
+    #jointRDD.foreach(print)
+
+    #what we have is (uga, (0, {M:1}))
+    #what we need is (0, (uga,{M:1}))
+    docIDFirstRDD=jointRDD.map(lambda x: (x[1][0], (x[0],x[1][1])))
+    #docIDFirstRDD.foreach(print)
+
+    docRDD = docIDFirstRDD.groupByKey().map(lambda x: (x[0],list(x[1])))
+    #docRDD.foreach(print)
+
+
+    catCount = dict(catCountRDD.collect())
+
+    def sumDictValues(d):
+        s = 0
+        for i in d:
+            s += d[i]
+        return s
+
+    totalNumberOfDocs = sumDictValues(catCount)
+    #print(totalNumberOfDocs)
+    #print(catCount)
+    totalNumberOfDocsBroadCast=sc.broadcast(totalNumberOfDocs)
+    catCountBroadCast=sc.broadcast(catCount)
+
+    def naiveBayes(x): #(docID, [(word1, {}), (word2, {}),....])
+        maxP = 0
+        maxCat = u'MCAT'
+
+        catCount = catCountBroadCast.value
+        totalNumberOfDocs=totalNumberOfDocsBroadCast.value
+
+        for cat in catCount:
+            p = catCount[cat] / float(totalNumberOfDocs)
+            for word in x[1]:
+                p *= word[1].get(cat,10e-7) / float(sumDictValues(word[1]))
+            if p >= maxP:
+                maxP = p
+                maxCat = cat
+        return (x[0],(maxCat,maxP))
+
+
+
+    predictionsRDD = docRDD.map(naiveBayes)
+    return predictionsRDD
+
 (wordCountByCatRDD,catCountRDD) = naive_bayes_train(X,Y)
 testDocument = u"Thursday night in the Oval Office with"
 testDocument2 = u"Friday Morning out of the Square Swimming Pool without"
 testRDD = sc.parallelize([testDocument,testDocument2]).zipWithIndex() # (testDocument, 0)
-testRDDSplit=testRDD.flatMap(lambda x: [(w,x[1]) for w in  x[0].lower().split(' ')])
-#testRDDSplit.foreach(print)
+# predictionsRDD = naive_bayes_predict(testingDatasetDocsRDD, wordCountByCatRDD, catCountRDD)
 
-jointRDD = testRDDSplit.join(wordCountByCatRDD)  #(uga,0).join((uga,{})) => (uga, (0,{})) 
-#wordCountByCat.join(testRDDSplit)     #(uga,{}).join((uga,0)) => (uga, ({},0))
-
-#jointRDD.foreach(print)
-
-print("----------")
-#what we have is (uga, (0, {M:1}))
-#what we need is (0, (uga,{M:1}))
-docIDFirstRDD=jointRDD.map(lambda x: (x[1][0], (x[0],x[1][1])))
-#docIDFirstRDD.foreach(print)
-
-docRDD = docIDFirstRDD.groupByKey().map(lambda x: (x[0],list(x[1])))
-#docRDD.foreach(print)
-
-
-catCount = dict(catCountRDD.collect())
-
-def sumDictValues(d):
-    s = 0
-    for i in d:
-        s += d[i]
-    return s
-
-totalNumberOfDocs = sumDictValues(catCount)
-print(totalNumberOfDocs)
-
-#print(catCount)
-
-totalNumberOfDocsBroadCast=sc.broadcast(totalNumberOfDocs)
-catCountBroadCast=sc.broadcast(catCount)
-
-def naiveBayes(x): #(docID, [(word1, {}), (word2, {}),....])
-    maxP = 0
-    maxCat = u'MCAT'
-
-    catCount = catCountBroadCast.value
-    totalNumberOfDocs=totalNumberOfDocsBroadCast.value
-
-    for cat in catCount:
-        p = catCount[cat] / float(totalNumberOfDocs)
-        for word in x[1]:
-            p *= word[1].get(cat,10e-7) / float(sumDictValues(word[1]))
-        if p >= maxP:
-            maxP = p
-            maxCat = cat
-    return (x[0],(maxCat,maxP))
-
-
-
-predictionsRDD = docRDD.map(naiveBayes)
+predictionsRDD = naive_bayes_predict(testRDD,wordCountByCatRDD, catCountRDD)
 
 predictionsRDD.foreach(print)
-import sys
-
-sys.exit(0)
 
 
-
-
-
-def splitter(x):
-    return [(w,1) for w in x.lower().split(' ')] #better tokenizer + stopwords removal
-Xsplit = X.flatMap(splitter) #not map!
-Xreduced = Xsplit.reduceByKey(lambda x,y: x+y)
-
-VocabularySize = Xreduced.count()
-#print(VocabularySize)
+#accuracy = score(predictionsRDD, testingDatasetLabelsRDD)
 
